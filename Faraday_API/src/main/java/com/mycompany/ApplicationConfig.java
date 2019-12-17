07d46d8452dfddeac05ec3f0cffdca3099224c5b
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2019-12-17 13:08:41 +0100

    somewhat working pipeline impl for faraday api

--- Faraday_API/src/main/java/com/mycompany/ApplicationConfig.java
+++ Faraday_API/src/main/java/com/mycompany/ApplicationConfig.java
@@ -31,7 +31,6 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.mycompany.ApiResource.class);
-        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
     }
 
 }
