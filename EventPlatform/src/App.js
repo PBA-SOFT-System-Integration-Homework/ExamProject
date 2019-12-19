fe6502b1213b2dc2c9bd3c0f3936b255be9a4ca9
Zurina <orchidevej@hotmail.com>
2019-12-19 11:50:47 +0100

    sry for spam

--- EventPlatform/src/App.js
+++ EventPlatform/src/App.js
@@ -78,7 +78,7 @@ class App extends React.Component {
       let response = await EventFacade.addEvent(event[0]);
       if (response.error) alert(response.error)
       else {
-        event[0]['amount_of_people'] = event.amountOfPeople
+        event[0]['amount_of_people'] = event[0].amountOfPeople
         delete event[0].amountOfPeople
         delete event[0].carType 
         delete event[0].numberOfSeats
