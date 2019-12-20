02bd48f7df0b4dfa62ea67f15467b200bfca0801
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2019-12-20 10:57:35 +0100

    added testing back to eventplatform backend

--- EventPlatformBackend/api/controllers/auth.controller.js
+++ EventPlatformBackend/api/controllers/auth.controller.js
@@ -8,7 +8,6 @@ const login = async (username, password) => {
     if (!username || !password || username === "" || password === "") {
         return { error: 'Username or password is missing' }
     }
-    
     try {
         const result = await loginAuth(username, password);
         // If an error occurs in db (they are not stacktraces but handled error messages)
